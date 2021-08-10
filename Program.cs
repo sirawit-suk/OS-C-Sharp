eb3e7d58eaa1b332511b0dfa6f9ac6aa54a1d8c2
Sirawit <popeye-zz@hotmail.com>
2021-08-10 18:56:48 +0700

    first save

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CshapeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is a first project");
+        }
+    }
+}
